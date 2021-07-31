36de5f557d63957dd2452168e70501a8d001ea4c
Nori Hamamoto <nhamamoto@bandainamcoent.com>
2021-07-31 15:29:55 -0700

    moved the main html contents from App.vue to Home.vue to let App.vue handle only the routing function.

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,21 @@
 <template>
-<div id="app">
-  <div id="nav">
-    <div class="menu-button pt-3 mr-3" @click.stop="openHistory">
-      <Hamburger></Hamburger>
-    </div>
-    <transition name="right">
-      <div id="history" v-if="historyShown">
-        <History @close="closeHistory"></History>
-      </div>
-    </transition>
-  </div>
-  <router-view/>
-</div>
+<router-view/>
 </template>
 
 <script>
-import Hamburger from '@/components/Hamburger.vue'
-import History from '@/components/History.vue'
 export default {
-  components: { Hamburger, History },
+  components: {},
   props: [ ],
   data: function(){
     return {
-      historyShown: false,
     }
   },
   computed: {
   },
   methods: {
-    openHistory: function(){
-      this.historyShown = true
-    },
-    closeHistory: function(){
-      this.historyShown = false
-    }
   }
 }
 </script>
 
 <style scoped>
-#app {
-  height: 100%;
-}
-
-#nav {
-    text-align: right;
-}
-
-.menu-button {
-  position: relative;
-  height: 20px;
-  width: 28px;
-  display: inline-block;
-}
-
-#history{
-  text-align: left;
-  position: absolute;
-  z-index: 10;
-  top: 0;
-  right: 0;
-  width: 50%;
-  height: 100%;
-  background-color: #eeeeff;
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,53 @@
 <template>
-<div class="home">
-  <iframe frameborder="0" :src="url"></iframe>
+<div id="app">
+  <div id="header">
+    <b-nav>
+      <b-nav-item @click="activePage = 'Weblio'">Weblio</b-nav-item>
+    </b-nav>
+
+    <div class="menu-button pt-3 mr-3" @click.stop="openHistory">
+      <Hamburger></Hamburger>
+    </div>
+
+    <transition name="right">
+      <div id="history" v-if="historyShown">
+        <History @close="closeHistory"></History>
+      </div>
+    </transition>
+  </div>
+
+  <div class="home">
+    <iframe v-if="activePage == 'Weblio'" :src="urlWeblio" frameborder="0"></iframe>
+  </div>
 </div>
 </template>
 
 <script>
+import Hamburger from '@/components/Hamburger.vue'
+import History from '@/components/History.vue'
 import _ from "lodash"
 
 export default {
-  components: { },
+  components: { Hamburger, History },
   props: [ ],
   data: function(){
     return {
+      historyShown: false,
+      activePage: "Weblio",
     }
   },
   computed: {
-    url: function(){
+    urlWeblio: function(){
       return "https://ejje.weblio.jp/content/" + (this.$route.params.word || "");
     }
   },
   methods: {
+    openHistory: function(){
+      this.historyShown = true
+    },
+    closeHistory: function(){
+      this.historyShown = false
+    }
   },
   created(){
     var storage = localStorage;
@@ -40,5 +68,35 @@ export default {
 .home, .home > iframe {
   height: 100%;
   width: 100%;
+}
+
+#app {
+  height: 100%;
+}
+
+#header {
+    text-align: right;
+}
+
+.nav {
+    float: left;
+}
+
+.menu-button {
+  position: relative;
+  height: 20px;
+  width: 28px;
+  display: inline-block;
+}
+
+#history{
+  text-align: left;
+  position: absolute;
+  z-index: 10;
+  top: 0;
+  right: 0;
+  width: 50%;
+  height: 100%;
+  background-color: #eeeeff;
 }
 </style>
