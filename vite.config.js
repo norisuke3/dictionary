f421bf6c823c9581d88c4aca51fcdbab5658dac3
Noriaki Hamamoto <norisuke3@gmail.com>
2024-10-25 14:51:07 -0700

    Add missing semicolons in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
