7a6cb0d5e8bb79e32cdce002612a04f8c87b0afd
Noriaki Hamamoto <norisuke3@gmail.com>
2024-10-29 19:43:23 -0700

    added more uttarance feature.

--- src/components/History.vue
+++ src/components/History.vue
@@ -5,8 +5,9 @@
       <b-row class="d-flex justify-content-between align-items-center h-100">
         <b-col class="d-flex justify-content-start ms-1"><div @click="close">→</div></b-col>
         <b-col class="d-flex justify-content-end align-items-center">
-          <div class="me-3 trash" @click="toggleDelete"><IBiTrash></IBiTrash></div>
+          <div class="me-3" @click="toggleSpeaker"><IF7Speaker3Fill></IF7Speaker3Fill></div>
           <div class="me-3" @click="toggleDate"><IBiCalendarDate></IBiCalendarDate></div>
+          <div class="me-3 trash" @click="toggleDelete"><IBiTrash></IBiTrash></div>
           <div @click="settingShown = true"><IBiGear></IBiGear></div>
         </b-col>
       </b-row>
@@ -16,10 +17,13 @@
       <template v-for="item in historyItems" :key="item.timestamp">
         <b-list-group-item :href="url(item.word)" class="py-0 icons words d-flex justify-content-between align-items-center">
           <div class="item">{{ item.word }}</div>
-          <div v-if="deleteShown" class="close text-center" @click.prevent="remove(item)">
+          <div v-if="deleteShown" class="item-icon text-center" @click.prevent="remove(item)">
             <span aria-hidden="true"><IMakiCross></IMakiCross></span>
           </div>
           <div v-if="dateShown" class="">{{ timestampToDate(item.timestamp) }}</div>
+          <div v-if="speakerShown" @click.prevent="utter(item.word)" class="item-icon text-center">
+            <IF7Speaker3Fill></IF7Speaker3Fill>
+          </div>
         </b-list-group-item>
       </template>
     </b-list-group>
@@ -46,6 +50,7 @@ export default {
       deleteShown: false,
       dateShown: false,
       settingShown: false,
+      speakerShown: true,
       storage: null,
       max: 100
     }
@@ -76,10 +81,20 @@ export default {
     toggleDelete: function(){
       this.deleteShown = !this.deleteShown;
       this.dateShown = false;
+      this.speakerShown = false;
     },
     toggleDate: function(){
       this.dateShown = !this.dateShown;
       this.deleteShown = false;
+      this.speakerShown = false;
+    },
+    toggleSpeaker: function(){
+      this.speakerShown = !this.speakerShown;
+      this.deleteShown = false;
+      this.dateShown = false;
+    },
+    utter: function(word){
+      utils.utter(word);
     }
   },
   async created(){
@@ -98,7 +113,7 @@ export default {
     line-height: 2.7rem;
 }
 
-.words > .close {
+.words > .item-icon {
     width: 1.7rem;
 }
 
